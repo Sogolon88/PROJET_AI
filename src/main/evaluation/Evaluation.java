a8a9f1126c53b8fa4c1e95858f058158cce1b322
jordanalaicrisosto-cyber <jordanalaicrisosto@gmail.com>
2026-02-12 20:59:50 +0100

    Début de réflexion sur l'évaluation

---
+++ src/main/evaluation/Evaluation.java
@@ -0,0 +1,17 @@
+package main.evaluation;
+
+public class Evaluation {
+
+    //méthode distance
+    //méthode qui évalue le score en fonction de la distance (et eventuellement d'un pion passé)
+    
+    //méthode qui score les pions avancés
+
+    //Méthode qui score la majorité par aile (gauche, centre, droite)
+
+    //Initiative (force une réponse, menace) (trop dur ?)
+
+    //méthode evaluation de structure (chaine de pions, mur)
+
+    //méhode malus (pion isolé pas passé, colonne vide, trou au centre)
+}
